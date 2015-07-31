75f751979741e6128870cc5fcee6e3e90eb9d6aa
Jason Carroll <jason@champ.net>
2015-07-31 16:02:47 -0500

    using svg logo

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,10 @@ $(window).scroll(function() {
     if ($(document).scrollTop() > 150) {
         $('nav').addClass('shrink navbar-default navbar-fixed-top');
         $('nav').removeClass('navbar-inverse');
-        $('#logo').attr('src', 'img/midtown-church-logo-primary.png');
+        $('#logo').attr('src', 'img/midtown-church-austin-logo-vector.svg');
     } else {
         $('nav').addClass('navbar-inverse navbar-static-top');
         $('nav').removeClass('shrink navbar-default navbar-fixed-top');
-        $('#logo').attr('src', 'img/midtown-church-central-austin-tx.png');
+        $('#logo').attr('src', 'img/midtown-church-austin-logo-white-vector.svg');
     }
 });
