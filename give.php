7f669f732667f7f4b130d726e00becbd61410e31
Jason Carroll <jason@champ.net>
2015-08-02 07:02:33 -0500

    added giving page

---
+++ give.php
@@ -0,0 +1,72 @@
+<?php include('header.html'); ?>
+
+<!-- Page Heading -->
+<!-- Parallax Section -->
+
+<section class="parallax parallax-giving">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="shadow-text">Giving &mdash; An Act of Worship</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /.page heading -->
+
+<!-- Page Content -->
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1 text-center">
+                <p class="lead">Jesus is the most generous person who ever lived. He gave his life so that we might live. As Christians, we give our time, talent, and money joyfully in response to Jesus’ generosity and to help more people meet Jesus.</p>
+
+                <p>As a community, we take joy in the ways our faith is made stronger through giving and receiving together. There is a satisfaction that comes from following Jesus’ example of generosity toward those most in need, and it is a privilege to use our resources and time to continue telling God’s story and help bring measurable change in the world.</p>
+
+                <p>Below are some ways for you to give to the ministry of Midtown Church.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-light-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h2 class="h-lines hl-2">Ways to Give</h2>
+            </div>
+            <div class="col-sm-6 text-center">
+                <h2>Online</h2>
+                <p>You can give via credit card or bank withdrawal either one time or recurring. Click the button below to begin.</p>
+                <p><a href="https://midtownchurch.cloverdonations.com/give/" target="_blank" class="btn btn-success btn-lg">Click Here to Give Online</a></p>
+            </div>
+            <div class="col-sm-6 text-center">
+                <h2>Cash &amp; Checks</h2>
+                <p>Please make checks payable to Midtown Church and drop them in the offering plate on Sunday or mail to:</p>
+                <address>
+                    Midtown Church<br>
+                    4809 Sinclair Ave<br>
+                    Austin, TX 78756
+                </address>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h2 class="h-gray h-lines hl-1">Volunteering</h2>
+                <p>If you’ve been served by Midtown Church, whether it be through greeters on Sundays, teachers in you child’s classroom, enjoying a cup of coffee before service, musicians on stage leading us in worship, or any number of other ways—chances are you’ve been served by a faithful volunteer giving up time and talent to serve Jesus by serving you. At Midtown, we intentionally keep our budget and staff small and rely on a community of volunteers who love Jesus and love to serve his church.</p>
+                <p>If you consider Midtown your church, we ask you to serve as a volunteer. By doing so, you’re an essential part of helping others meet Jesus. And if you’re a member, you’ve committed to serve generously as part of your membership covenant.</p>
+                <p>There are a number of opportunities to serve at Midtown. To find out how you can serve, contact Adam Brunson at adam@midtownaustin.org</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.html'); ?>
