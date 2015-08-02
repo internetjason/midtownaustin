606a7444287c81b5830692237209220ef1c6f0ab
Jason Carroll <jason@champ.net>
2015-08-02 14:02:12 -0500

    fixed shrink navbar js

--- midtown/assets/js/custom.js
+++ midtown/assets/js/custom.js
@@ -2,10 +2,12 @@ $(window).scroll(function() {
     if ($(document).scrollTop() > 150) {
         $('nav').addClass('shrink navbar-default navbar-fixed-top');
         $('nav').removeClass('navbar-inverse');
-        $('#logo').attr('src', 'img/midtown-church-austin-logo-vector.svg');
+        $('#logo').removeClass('white-logo');
+        $('#logo').addClass('color-logo');
     } else {
         $('nav').addClass('navbar-inverse navbar-static-top');
         $('nav').removeClass('shrink navbar-default navbar-fixed-top');
-        $('#logo').attr('src', 'img/midtown-church-austin-logo-white-vector.svg');
+        $('#logo').removeClass('color-logo');
+        $('#logo').addClass('white-logo');
     }
 });
