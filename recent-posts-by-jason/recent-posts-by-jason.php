13820c4031726339aba5ad037a7598147d47fbd3
Jason Carroll <jason@champ.net>
2015-08-21 13:57:02 -0600

    added image alt

--- recent-posts-by-jason/recent-posts-by-jason.php
+++ recent-posts-by-jason/recent-posts-by-jason.php
@@ -75,8 +75,15 @@ if(!class_exists('rpbj'))
                 $output .= '';
                 while ( $rpbj_query->have_posts() ) {
                     $rpbj_query->the_post();
-                    $output .= '<div class="' . $container . '"><h4><a href="' . get_permalink() . '">' . get_the_title() . '</a></h4>';
-                    if ($image) { $output .= '<p><a href="' . get_permalink() . '"><img src="' . wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'thumbnail') ) . '" class="img-responsive img-thumbnail"></a></p>'; };
+                    
+		    $thumbnail = get_post( get_post_thumbnail_id() );
+                    $alt = get_post_meta( $thumbnail->ID, '_wp_attachment_image_alt', true );
+
+		    $output .= '<div class="' . $container . '"><h4><a href="' . get_permalink() . '">' . get_the_title() . '</a></h4>';
+                    if ($image) { $output .= '<p><a href="' . get_permalink() . '"><img 
+			src="' . wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'thumbnail') ) . '" 
+			class="img-responsive img-thumbnail"
+			alt="' . $alt  . '"</a></p>'; };
                     if ($meta) { $output .= '<i class="fa fa-calendar"></i> <time class="podcast-meta">' . get_the_date() . '</time>'; };
                     if ($excerpt) { $output .= '<p>' . get_the_excerpt() . '</p>'; };
                     $output .= '<a href="' . get_permalink() . '" class="btn ' . $buttonclass . '">' . $buttontext .'</a>';
