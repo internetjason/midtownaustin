e53609ec9c27226ac96b0e094b4cd73fab82a63e
Jason Carroll <jason@champ.net>
2015-08-21 13:56:36 -0600

    added google analytics code

--- midtown/footer.php
+++ midtown/footer.php
@@ -23,6 +23,15 @@
         <script src="<?php echo bloginfo('template_directory'); ?>/assets/js/custom.js"></script>
 
 <?php if ( is_singular() ) wp_print_scripts( 'comment-reply' ); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-43425152-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
     </body>
 </html>
