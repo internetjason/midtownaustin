65291ded18bf20db4a2de602ae0c4def8e709fcb
Jason Carroll <jason@champ.net>
2015-09-08 12:58:21 -0600

    attempting to fix gravity forms

--- midtown/footer.php
+++ midtown/footer.php
@@ -23,6 +23,7 @@
         <script src="<?php echo bloginfo('template_directory'); ?>/assets/js/custom.js"></script>
 
 <?php if ( is_singular() ) wp_print_scripts( 'comment-reply' ); ?>
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
